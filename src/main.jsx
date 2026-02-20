488857016a33ab8eb67ded48afba56fc1bca2fa3
Reuben <ftreuben1520@gmail.com>
2026-02-20 17:56:29 +0530

    fix: add missing React/ReactDOM/App imports to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
 // import { initScrollEffects } from './scrollEffects';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
