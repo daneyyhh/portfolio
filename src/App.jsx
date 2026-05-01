d03070991552cb9a32448cf905e02aa56ef8913b
Reuben <ftreuben1520@gmail.com>
2026-05-01 10:33:26 +0530

    Update portfolio with spider cursor and integrated sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cursor from './Cursor';
 import HeroReveal from './components/Sections/HeroReveal';
 import OriginSection from './components/Sections/OriginSection';
 import ComicScrollSection from './components/Sections/ComicScrollSection';
 import FaceOffSection from './components/Sections/FaceOffSection';
 import TransitionCtaSection from './components/Sections/TransitionCtaSection';
+import About from './components/Sections/About';
+import Projects from './components/Sections/Projects';
+import Contact from './components/Sections/Contact';
 import SeoPage from './components/Sections/SeoPage';
 import NotFound from './components/Sections/NotFound';
 
 function App() {
     return (
         <Router>
+            <Cursor />
             <Routes>
                 <Route path="/" element={
                     <>
@@ -19,6 +24,11 @@ function App() {
                         <ComicScrollSection />
                         <FaceOffSection />
                         <TransitionCtaSection />
+                        <div className="bg-gray-950">
+                            <About />
+                            <Projects />
+                            <Contact />
+                        </div>
                     </>
                 } />
                 <Route path="/seo" element={<SeoPage />} />
@@ -27,5 +37,7 @@ function App() {
         </Router>
     );
 }
+
+
 
 export default App;
